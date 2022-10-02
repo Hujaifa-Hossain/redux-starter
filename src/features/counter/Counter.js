820d31fecf18eca1b00dd689da2c07e85d8e93d8
Hujaifa21 <21hujaifa@gmail.com>
2022-10-03 00:58:51 +0600

    again implemented the same code without doc

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import { useDispatch, useSelector, } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./counterSlice";
 
 const Counter = () => {
-  // const count = useSelector((state) => {state.counter.value});
-  const count = useSelector((state) => state.counter.value)
+  const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
   return (
-    <section>
-      <h3>{count}</h3>
+    <div>
+      <h1>{count}</h1>
 
-      <div>
-        <button onClick={() => dispatch(increment())}>+</button>
-        <button onClick={() => dispatch(decrement())}>-</button>
-      </div>
-
-    </section>
+      <button onClick={() => dispatch(increment())}>Increment ++</button>
+      <button onClick={() => dispatch(decrement())}>Decrement --</button>
+    </div>
   );
 };
 
